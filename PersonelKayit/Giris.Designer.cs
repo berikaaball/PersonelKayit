1a5cea890b48dcf3ea0827f31fd2213cd6863e95
Berika Bal <berikaaball@gmail.com>
2024-11-29 18:49:30 +0300

    database file added

---
+++ PersonelKayit/Giris.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace PersonelKayit
+{
+    partial class Giris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TBoxSifre = new System.Windows.Forms.TextBox();
+            this.TBoxKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(55, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(116, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 22.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(481, 45);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Personel Takip Sistemi Giriş Paneli";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(184, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Giriş";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TBoxSifre
+            // 
+            this.TBoxSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TBoxSifre.Location = new System.Drawing.Point(184, 154);
+            this.TBoxSifre.Name = "TBoxSifre";
+            this.TBoxSifre.PasswordChar = '*';
+            this.TBoxSifre.Size = new System.Drawing.Size(234, 28);
+            this.TBoxSifre.TabIndex = 2;
+            this.TBoxSifre.UseSystemPasswordChar = true;
+            // 
+            // TBoxKullaniciAdi
+            // 
+            this.TBoxKullaniciAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TBoxKullaniciAdi.Location = new System.Drawing.Point(184, 117);
+            this.TBoxKullaniciAdi.Name = "TBoxKullaniciAdi";
+            this.TBoxKullaniciAdi.Size = new System.Drawing.Size(234, 28);
+            this.TBoxKullaniciAdi.TabIndex = 1;
+            // 
+            // Giris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(570, 330);
+            this.Controls.Add(this.TBoxKullaniciAdi);
+            this.Controls.Add(this.TBoxSifre);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Giris";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Giris";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TBoxSifre;
+        private System.Windows.Forms.TextBox TBoxKullaniciAdi;
+    }
+}
---
+++ PersonelKayit/Giris.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonelKayit
+{
+    public partial class Giris : Form
+    {
+        public Giris()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                VTIslem.baglanti.Open();
+                string kullanici = TBoxKullaniciAdi.Text;
+                string sifre = TBoxSifre.Text;
+                string giris = "SELECT * FROM Tbl_Giris WHERE KullaniciAdi=@kullanici AND Sifre=@sifre";
+                VTIslem.command.Parameters.Clear();
+                VTIslem.command.Parameters.AddWithValue("@kullanici", kullanici);
+                VTIslem.command.Parameters.AddWithValue("@sifre", sifre);
+                VTIslem.command.Connection = VTIslem.baglanti;
+                VTIslem.command.CommandText = giris;
+                SqlDataReader reader = VTIslem.command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    AnaForm afrm = new AnaForm();
+                    afrm.Show();
+                    this.Hide();
+
+                }
+                else
+                {
+                    MessageBox.Show("Hatalı kullanıcı adı veya şifre!", "Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                VTIslem.baglanti.Close();
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Beklenmedik bir hata var.","Uyarı",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+
+            
+        }
+    }
+}
